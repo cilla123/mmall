6427c0682f3db0e18d8caee3a7c8957a138d7852
Ben <892156860@qq.com>
2018-05-19 23:26:36 +0800

    提交用户登录

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,25 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+public interface IUserService {
+
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> forgetCheckAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetResetPassword(String username, String newPassword, String forgetToken);
+
+    ServerResponse<String> resetPassword(String oldPassword, String newPassword, User user);
+
+    ServerResponse<User> updateInformation(User user);
+
+    ServerResponse<User> getInformation(Integer userId);
+}
